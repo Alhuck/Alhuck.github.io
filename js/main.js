1f0ba56cd0d885ac0e3bcce199846423800859d5
Alhuck <alhucka@gmail.com>
2021-04-06 17:01:23 -0400

    Update resume and removed some functions

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
-$(document).ready(function() {
-    var urls = ['resources/images/one.png', 'resources/images/two.png', 'resources/images/three.png', 'resources/images/four.png', 'resources/images/five.png'];
-  
-    var cout = 1;
-    $('body').css('background-image', 'url("' + urls[0] + '")');
-    setInterval(function() {
-      $('body').css('background-image', 'url("' + urls[cout] + '")');
-      cout == urls.length-1 ? cout = 0 : cout++;
-    }, 5000);
-  
-  });
+$(document).ready(function () {
+  var urls = ['resources/images/one.png', 'resources/images/two.png', 'resources/images/three.png', 'resources/images/four.png', 'resources/images/five.png'];
+
+  var cout = 1;
+  $('body').css('background-image', 'url("' + urls[0] + '")');
+  // setInterval(function() {
+  //   $('body').css('background-image', 'url("' + urls[cout] + '")');
+  //   cout == urls.length-1 ? cout = 0 : cout++;
+  // }, 5000);
+
+});
